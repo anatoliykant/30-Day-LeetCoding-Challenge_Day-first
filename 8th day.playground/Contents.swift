b325cd4924f1e2010903fddc2478167c1e6be519
Anatoliy Podkladov <kant.anatoliy@yandex.ru>
2020-04-09 00:15:38 +0300

    Updated description

--- 8th day.playground/Contents.swift
+++ 8th day.playground/Contents.swift
@@ -3,7 +3,7 @@
 /// -----------------------------------------------
 /// https://leetcode.com/explore/other/card/30-day-leetcoding-challenge
 
-// Day eighth
+// Day eighth (Weak 2 Day first)
 //
 // https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3290/
 //
