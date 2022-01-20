889a392d8cfa734cc89ee88af46403f864af7bb5
Anatoliy Kant <kant.anatoliy@yandex.ru>
2022-01-20 16:26:18 +0300

    Update Contents.swift

--- 4th day.playground/Contents.swift
+++ 4th day.playground/Contents.swift
@@ -5,21 +5,9 @@
 
 // Day fouth
 //
-// https://leetcode.com/problems/maximum-subarray/
+// https://leetcode.com/problems/move-zeroes/
 //
-//    Maximum Subarray
-//
-//    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
-//
-//    Example:
-//
-//    Input: [-2,1,-3,4,-1,2,1,-5,4],
-//    Output: 6
-//    Explanation: [4,-1,2,1] has the largest sum = 6.
-//    Follow up:
-//
-//    If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
-
+//    Move Zeroes
 //    Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 //
 //    Example:
