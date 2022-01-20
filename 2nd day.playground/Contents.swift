26813fe3f59930f6d3a6c799792b49f124880098
Anatoliy Kant <kant.anatoliy@yandex.ru>
2022-01-20 16:27:46 +0300

    Update Contents.swift

--- 2nd day.playground/Contents.swift
+++ 2nd day.playground/Contents.swift
@@ -5,8 +5,9 @@
 
 // Day second
 //
-// https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3284/
+// https://leetcode.com/problems/happy-number/
 //
+// Happy Number
 // Write an algorithm to determine if a number is "happy".
 //
 // A happy number is a number defined by the following process: Starting with  any positive integer, replace the number by the sum of the squares of its  digits, and repeat the process until the number equals 1 (where it will  stay), or it loops endlessly in a cycle which does not include 1. Those  numbers for which this process ends in 1 are happy numbers.
