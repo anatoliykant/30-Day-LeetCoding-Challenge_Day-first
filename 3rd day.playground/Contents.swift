5272e0ac3411c4e9124db6070645491d8f799aa1
Anatoliy Kant <kant.anatoliy@yandex.ru>
2022-01-20 16:26:20 +0300

    Update Contents.swift

--- 3rd day.playground/Contents.swift
+++ 3rd day.playground/Contents.swift
@@ -5,7 +5,7 @@
 
 // Day third
 //
-// https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3285/
+// https://leetcode.com/problems/maximum-subarray/
 //
 //    Maximum Subarray
 //    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
