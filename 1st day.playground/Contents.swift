6151d41eea57c0a59a6bf4aeb84953243ea8ae82
Anatoliy Kant <kant.anatoliy@yandex.ru>
2022-01-20 16:28:47 +0300

    Update Contents.swift

--- 1st day.playground/Contents.swift
+++ 1st day.playground/Contents.swift
@@ -7,6 +7,7 @@
 //
 // https://leetcode.com/problems/single-number/
 //
+// Single Number
 // Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 //
 // Note:
